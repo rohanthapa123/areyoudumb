8e9431b21569518bd509ead5547a97975acb020a
Rohan Thapa <thaparohan2019@gmail.com>
2023-03-31 11:34:39 +0545

    update

--- index.js
+++ index.js
@@ -5,13 +5,15 @@ let result = document.querySelector(".result")
 function changePosition(){
 
     let x =Math.floor(Math.random()*100);
-    let y =Math.round(Math.random());
-    let z =Math.round(Math.random());
+    let y =Math.round(Math.random(100));
+    let z =Math.round(Math.random(100));
     console.log(x,y,z)
-    y == 0 ? no.style.left = x+"%" : no.style.right = x+"%"
-    z == 1 ? no.style.top = x+"%" : no.style.buttom = x+"%"
+    no.style.background = `rgba(${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)})`;
+    y%2 == 0 ? no.style.left = x+"%" : no.style.right = x+"%"
+    z%2 == 0 ? no.style.top = x+"%" : no.style.buttom = x+"%"
     // no.style.horizon[y] = x+"px";
     // no.style.vertical[z] = x+"px";
+    // no.style.transform = `translate(${x}%,${x}%)`
 
 }
 yes.addEventListener('click',()=>{
